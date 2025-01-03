c99aa55ae692e2333e55e3f9280c672050c456e9
Ramashish Sahani <ramashishsahani786@gmail.com>
2025-01-03 13:26:04 +0530

    Updated App

--- Dashboard/App.py
+++ Dashboard/App.py
@@ -15,10 +15,20 @@ df = pd.read_csv(data_file,encoding='unicode_escape')
 df['Order Date'] = pd.to_datetime(df['Order Date'])
 df['Ship Date'] = pd.to_datetime(df['Ship Date'])
 
+
+
 st.set_page_config(layout="wide")
 st.markdown('<style>div.bloack-container{padding-top:0.1rem;}</style>',unsafe_allow_html=True)
 
+
 image = Image.open("../Data/superstore.png")
+
+
+with st.sidebar:
+    st.write("Here will filter")
+
+box_date = str(datetime.datetime.now().strftime("%d %B %Y"))
+st.write(f"Last Updated : {box_date}")
 
 col1,col2 = st.columns([0.1,0.9])
 
@@ -39,13 +49,10 @@ birder-radius:6px;
 with col2:
     st.markdown(html_title,unsafe_allow_html=True)
 
-col3,col4,col5 = st.columns([0.1,0.45,0.45])
+col3,col4 = st.columns([0.5,0.5])
+
 
 with col3:
-    box_date = str(datetime.datetime.now().strftime("%d %B %Y"))
-    st.write(f"Last Updated :  \n {box_date}")
-
-with col4:
     fig = px.bar(df,x="Ship Mode",y="Sales",labels={"Sales":"Total Sales ($)"},
                  title="Total Sales by Ship Mode",hover_data=['Sales'],template="gridon",height=500,text='Sales')
     fig.update_traces(texttemplate='%{text:.2s}', textposition='outside')
@@ -60,3 +67,19 @@ with view1:
     expander.write(data)
 with dwn1:
     st.download_button("Get Data",data=data.to_csv().encode('utf-8'),file_name='sales_by_ship_mode.csv',mime='text\csv')
+
+
+with col4:
+    fig = px.bar(df,x="Category",y="Sales",labels={"Sales":"Total Sales ($)"},
+                 title="Total Sales by Category",hover_data=['Sales'],template="gridon",height=500,text='Sales')
+    fig.update_traces(texttemplate='%{text:.2s}', textposition='outside')
+    fig.update_layout(uniformtext_minsize=8, uniformtext_mode='hide')
+    st.plotly_chart(fig,use_container_width=True)
+
+
+with view2:
+    expander = st.expander("Sales by Ship Mode")
+    data = df[["Category","Sales"]].groupby(by='Category')['Sales'].sum()
+    expander.write(data)
+with dwn2:
+    st.download_button("Get Data",data=data.to_csv().encode('utf-8'),file_name='sales_by_category.csv',mime='text\csv')
--- Dashboard/test.py
+++ Dashboard/test.py
@@ -5,12 +5,18 @@ data_file = "../Data/superstore.csv"
 df = pd.read_csv(data_file,encoding='unicode_escape')
 
 
+
 df['Order Date'] = pd.to_datetime(df['Order Date'])
 df['Ship Date'] = pd.to_datetime(df['Ship Date'])
 
 list_columns = df.columns
 print(list_columns)
 
-data = df[['Ship Mode','Sales']].groupby(by='Ship Mode')['Sales'].sum()
+# # Sales by ship mode
+# data = df[['Ship Mode','Sales']].groupby(by='Ship Mode')['Sales'].sum()
+# print(data)
 
+# Sales by  country
+
+data = df[['Category','Sales']].groupby(by='Category')['Sales'].sum()
 print(data)
