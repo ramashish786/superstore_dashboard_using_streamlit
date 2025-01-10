eb79f8448e29ac74a9364cbd812973a22ca7a5ff
Ramashish Sahani <ramashishsahani786@gmail.com>
2025-01-10 10:33:19 +0530

    Uodated App

--- Dashboard/App.py
+++ Dashboard/App.py
@@ -22,7 +22,7 @@ df['Month'] = pd.DatetimeIndex(df['Order Date']).month
 ## Adding Year-Month column in dataframe
 df['Year-Month'] = df['Year'].astype(str) +'-'+  df['Month'].astype(str)
 
-
+filters =[]
 
 st.set_page_config(layout="wide")
 st.markdown('<style>div.bloack-container{padding-top:0.1rem;}</style>',unsafe_allow_html=True)
@@ -32,7 +32,22 @@ image = Image.open("../Data/superstore.png")
 
 
 with st.sidebar:
-    st.write("Here will filter")
+    st.write("## Filter")
+    options = st.multiselect(
+        "Select Year",
+        [2014,2015,2016,2017]      
+    )
+    filters = [int(i) for i in options]
+    st.write(options)
+
+
+
+if len(filters) > 0:
+    tempDF = pd.DataFrame(columns=df.columns)
+    for i in range(0,len(filters),1):
+        tempDF = pd.concat([tempDF,df[df['Year'] == filters[i]]],ignore_index=True)
+    df = tempDF
+
 
 box_date = str(datetime.datetime.now().strftime("%d %B %Y"))
 st.write(f"Last Updated : {box_date}")
@@ -53,6 +68,8 @@ birder-radius:6px;
 <center><h1 class="main-title"> Superstore Dashboard </center> 
 
 """
+
+
 with col2:
     st.markdown(html_title,unsafe_allow_html=True)
 
@@ -89,7 +106,7 @@ with col4:
 
 
 with view2:
-    expander = st.expander("Sales by Ship Mode")
+    expander = st.expander("Sales by Category")
     data = df[["Category","Sales"]].groupby(by='Category')['Sales'].sum()
     expander.write(data)
 with dwn2:
@@ -104,7 +121,7 @@ _,view3, dwn3 , view4 , dwn4 = st.columns([0.15,0.20,0.20,0.20,0.20])
 # Sales by region : 
 with col5:
     fig = px.bar(df,x="Region",y="Sales",labels={"Sales":"Total Sales ($)"},
-                 title="Total Sales by Category",hover_data=['Sales'],template="gridon",height=500,text='Sales')
+                 title="Total Sales by Region",hover_data=['Sales'],template="gridon",height=500,text='Sales')
     fig.update_traces(texttemplate='%{text:.2s}', textposition='outside')
     fig.update_layout(uniformtext_minsize=8, uniformtext_mode='hide')
     st.plotly_chart(fig,use_container_width=True)
@@ -117,21 +134,44 @@ with dwn3:
     st.download_button("Get Data",data=data.to_csv().encode('utf-8'),file_name="Sales_by_Region.csv",mime='text\csv')
 
 
+with col6:
+    data = df[['Region','Profit']].groupby(by=['Region']).sum()['Profit'].round(2)
+    data = data.reset_index()
+    fig = px.bar(data,x="Region",y="Profit",labels={"Sales":"Total Profit ($)"},
+                 title="Total Sales by Region",hover_data=['Profit'],template="gridon",height=500,text='Profit')
+    fig.update_traces(texttemplate='%{text:.2s}', textposition='outside')
+    fig.update_layout(uniformtext_minsize=8, uniformtext_mode='hide')
+    st.plotly_chart(fig,use_container_width=True)
+
+
+with view4:
+    expander = st.expander('Profit by Region')
+    data = df[["Region","Profit"]].groupby(by='Region')["Profit"].sum()
+    expander.write(data)
+
+with dwn4:
+    st.download_button("Get Data",data=data.to_csv().encode('utf-8'),file_name="Profit_by_Region.csv",mime='text\csv')
+
+
+
 _,col7 = st.columns([0.1,0.9])
 
 
 
 with col7:
-    data = df[['Year-Month','Sales']].groupby(by=['Year-Month']).sum()['Sales']
+    data = df[['Year-Month','Sales']].groupby(by=['Year-Month']).sum()['Sales'].round(2)
     data = data.reset_index()
-    fig = px.bar(data,x="Year-Month",y='Sales')
+    fig = px.bar(data,x="Year-Month",y='Sales',labels={"Sales":"Total Sales ($)"},
+                 title="Yearly Sales",hover_data=['Sales'],template="gridon",height=500,text="Sales")
+    fig.update_traces(texttemplate='%{text:.2s}', textposition='outside')
+    fig.update_layout(uniformtext_minsize=8, uniformtext_mode='hide')
     st.plotly_chart(fig,use_container_width=True)
 
 _,view5, dwn5,_= st.columns([0.30,0.20,0.20,0.30])
 
 with view5:
     expander = st.expander("Sales by Year Month")
-    data = df[['Year-Month','Sales']].groupby(by=['Year-Month']).sum()['Sales']
+    data = df[['Year-Month','Sales']].groupby(by=['Year-Month']).sum()['Sales'].round(2)
     data = data.reset_index()
     expander.write(data)
 with dwn5:
--- Dashboard/test.py
+++ Dashboard/test.py
@@ -24,11 +24,16 @@ df['Month'] = pd.DatetimeIndex(df['Order Date']).month
 df['Year-Month'] = df['Year'].astype(str) +'-'+  df['Month'].astype(str)
 
 
-data = df[['Year-Month','Sales']].groupby(by=['Year-Month']).sum()['Sales']
-data = data.reset_index()
-plt.plot(data['Year-Month'],data['Sales'])
-plt.show()
+# data = df[['Year-Month','Sales']].groupby(by=['Year-Month']).sum()['Sales']
+# data = data.reset_index()
+# plt.plot(data['Year-Month'],data['Sales'])
+# plt.show()
 # data = data.unstack()
 
+fl = [2014,2015]
+tempDF = pd.DataFrame(columns=df.columns)
+for i in range(0,len(fl),1):
+    tempDF = pd.concat([tempDF,df[df['Year'] == fl[i]]],ignore_index=True)
 
-print(data.head())
+
+print(tempDF.tail())
